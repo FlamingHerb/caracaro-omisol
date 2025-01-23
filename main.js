6f80131f2a6d23040c405da43c3496e93bb029c8
FlamingHerb <msbbomisol@gmail.com>
2025-01-23 17:45:19 +0800

    chore: added comments to functions

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// The API
+const api = "https://pokeapi.co/api/v2/pokemon/";
+
 // Document Elements
 const searchBar = document.querySelector("input");
 const images = document.getElementById("pokemonimages");
@@ -17,17 +20,20 @@ const pokemonSPATK = document.getElementById("pokemonspatk");
 const pokemonSPDEF = document.getElementById("pokemonspdef");
 const pokemonSPD = document.getElementById("pokemonspd");
 
-// The API
-const api = "https://pokeapi.co/api/v2/pokemon/";
+
 
 // Add immediately after script loaded. 
-
 searchBar.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+    // Press Enter in order to load script.
+    if (e.code === "Enter") {
         getPokemonData(e);
     }
 });
 
+/**
+ * Requests for data from the API and sends them to setPokemonData().
+ * @param {string} pokeName - The requested name of the Pokemon.
+ */
 async function getPokemonData(pokeName) {
     // Making sure that the following URL request is case-insensitive.
     const newURLRequest = api.concat('', pokeName.target.value.toLowerCase());
@@ -45,7 +51,11 @@ async function getPokemonData(pokeName) {
         
         // JSON Obtained, proceed to change.
         console.log(json["name"]);
+
+        // Change table data.
         setPokemonData(json);
+
+        // Change hidden properties of elements.
         images.hidden = false;
         table.hidden = false;
         failmessage.hidden = true;
@@ -53,8 +63,9 @@ async function getPokemonData(pokeName) {
     
     // If not found, hide table.
     catch (error) {
-        // If none, hide table.
         console.error(error.message);
+
+        // Change hidden properties of elements.
         images.hidden = true;
         table.hidden = true;
         failmessage.hidden = false;
@@ -62,49 +73,55 @@ async function getPokemonData(pokeName) {
     }
 }
 
+/**
+ * Sets Pokemon data for viewing in a tabular form.
+ * @param {json} json - The Pokemon's Data retrieved from PokeAPI in JSON format.
+ */
 function setPokemonData(json){
-    if (json == ""){
-        pokemonFrontImage.src = "";
-    } 
-    else {
-        pokemonAbilitiesList.innerHTML = "";
-        pokemonType.innerHTML = "";
+    // Resetting types and abilities for iteration.
+    pokemonAbilitiesList.innerHTML = "";
+    pokemonType.innerHTML = "";
 
-        pokemonFrontImage.src       = json.sprites.front_default;
-        pokemonBackImage.src        = json.sprites.back_default;
-        pokemonName.innerHTML       = json.name[0].toUpperCase() + json.name.substring(1);
+    pokemonFrontImage.src       = json.sprites.front_default;
+    pokemonBackImage.src        = json.sprites.back_default;
+    pokemonName.innerHTML       = json.name[0].toUpperCase() + json.name.substring(1);
 
-        for (let index = 0;  index < json.types.length; index++) {
-            const typeName = json.types[index].type.name;
+    // Iterate over types.
+    for (let index = 0;  index < json.types.length; index++) {
+        const typeName = json.types[index].type.name;
 
-            pokemonType.innerHTML += typeName[0].toUpperCase() + typeName.substring(1);
+        // Title case the noun.
+        pokemonType.innerHTML += typeName[0].toUpperCase() + typeName.substring(1);
 
-            if ((index + 1) != json.types.length) {
-                pokemonType.innerHTML += ", "
-            }
+        // If there are more types, add a comma.
+        if ((index + 1) != json.types.length) {
+            pokemonType.innerHTML += ", "
         }
-
-        for (let index = 0;  index < json.abilities.length; index++) {
-            const abilityName = json.abilities[index].ability.name;
-
-            pokemonAbilitiesList.innerHTML += abilityName[0].toUpperCase() + abilityName.substring(1);
-
-            // Check if hidden. If hidden, then print the following.
-            if (json.abilities[index].is_hidden){
-                pokemonAbilitiesList.innerHTML += " (hidden)"
-            }
-
-            if ((index + 1) != json.abilities.length) {
-                pokemonAbilitiesList.innerHTML += ", "
-            }
-        }
-
-        pokemonHP.innerHTML         = json.stats[0].base_stat;
-        pokemonATK.innerHTML        = json.stats[1].base_stat;
-        pokemonDEF.innerHTML        = json.stats[2].base_stat;
-        pokemonSPATK.innerHTML      = json.stats[3].base_stat;
-        pokemonSPDEF.innerHTML      = json.stats[4].base_stat;
-        pokemonSPD.innerHTML        = json.stats[5].base_stat;
     }
-    
+
+    // Iterate over abilties.
+    for (let index = 0;  index < json.abilities.length; index++) {
+        const abilityName = json.abilities[index].ability.name;
+
+        // Title case the noun.
+        pokemonAbilitiesList.innerHTML += abilityName[0].toUpperCase() + abilityName.substring(1);
+
+        // Check if hidden. If hidden, then print the following.
+        if (json.abilities[index].is_hidden){
+            pokemonAbilitiesList.innerHTML += " (hidden)"
+        }
+
+        // If there are more abilities, add a comma.
+        if ((index + 1) != json.abilities.length) {
+            pokemonAbilitiesList.innerHTML += ", "
+        }
+    }
+
+    // Set remaining base stats.
+    pokemonHP.innerHTML         = json.stats[0].base_stat;
+    pokemonATK.innerHTML        = json.stats[1].base_stat;
+    pokemonDEF.innerHTML        = json.stats[2].base_stat;
+    pokemonSPATK.innerHTML      = json.stats[3].base_stat;
+    pokemonSPDEF.innerHTML      = json.stats[4].base_stat;
+    pokemonSPD.innerHTML        = json.stats[5].base_stat;
 }
